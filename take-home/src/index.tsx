293e6f728202006af4486d4535e4fb185a6159b7
Andrew Ploeger <andrewploeger@gmail.com>
2023-12-27 14:03:53 -0600

    added env to git ignore, also added scss module

--- take-home/src/index.tsx
+++ take-home/src/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const client = new ApolloClient({
+  uri: "https://www.google.com",
+  cache: new InMemoryCache(),
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
