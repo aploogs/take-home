fcd4d16bb5af065680d9c7ff2d86a04768f2e515
Andrew Ploeger <andrewploeger@gmail.com>
2024-01-17 21:04:13 -0600

    first commit, much broken

---
+++ take-home/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Home from "../components/home";
+import Layout from "./layout";
+
+function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
+
+export default App;
--- take-home/src/components/Home/index.tsx
+++ take-home/src/components/Home/index.tsx
@@ -1,25 +1,30 @@
+"use client";
 import React from "react";
 import { Grid } from "@mui/material";
 import NavBar from "../nav-bar";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 import RootCalculator from "../root-calculator";
 import Ticker from "../ticker";
-import styles from "./home.module.scss";
+import styles from "./styles/home.module.scss";
 
 const Home: React.FC = () => {
   return (
     <>
-      <Grid pt={1} container maxWidth={1900}>
-        <Grid item>
-          <NavBar />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Grid pt={1} container maxWidth={1900}>
+          <Grid item>
+            <NavBar />
+          </Grid>
+          <Grid className={styles.calculator} item lg={12}>
+            <RootCalculator />
+          </Grid>
+          <Grid className={styles.ticker} item lg={12}>
+            <Ticker />
+          </Grid>
         </Grid>
-        <Grid className={styles.calculator} item lg={12}>
-          <RootCalculator />
-        </Grid>
-        <Grid className={styles.ticker} item lg={12}>
-          <Ticker />
-        </Grid>
-      </Grid>
+      </LocalizationProvider>
     </>
   );
 };
